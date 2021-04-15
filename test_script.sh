fb2ae6146d0506fa13e6441a3082784208749ca9
Ubuntu <ubuntu@ip-172-31-20-237.ec2.internal>
2021-04-15 06:18:56 -0500

    edit >>

--- test_script.sh
+++ test_script.sh
@@ -2,7 +2,7 @@ cd ~/anntools
 touch results.txt
 for k in  data/free_1.vcf data/premium_2.vcf data/premium_2.vcf; do
 for i in {1..3}; do
-time python run.py $k > results.txt
+time python run.py $k >> results.txt
 done;
 done;
 
